641076e56581b199827df8f9ea4741ee0fbfa302
Mwitishi <mwitishi@gmail.com>
2014-07-22 10:20:59 +0200

    Added initialization, structures

--- galama.c
+++ galama.c
@@ -1,15 +1,41 @@
 #include "galama.h"
 
+//Constants (not all of them)
+const int inilives = 3;
+
 //State machine
 uint8_t state, level;
 int score, lives;
 
 SDL_Surface *screen = NULL;
+SDL_Surface *img_player = NULL;
+SDL_Surface *img_enemy[GAL_ENEMY_TYP];
+
+int gal_init() {
+    if(SDL_Init(SDL_INIT_EVERYTHING)) return 1;
+
+    screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
+    if(screen == NULL) return 1;
+
+    score = 0;
+    lives = inilives;
+    state = 0;
+
+    return 0;
+}
+
+int gal_quit() {
+    SDL_Quit();
+
+    return 0;
+}
 
 int main(int argc, char **argv) {
     
 
-    
+    gal_init();
+
+    gal_quit();
 
     return 0;
 }
--- galama.h
+++ galama.h
@@ -5,7 +5,31 @@
 #include <SDL/SDL_ttf.h>
 
 #define GAL_HISCORE_FILE "res/hiscores.dat"
+#define GAL_BUTTON_MAXLEN 30
+#define GAL_ENEMY_TYP 1
+
+typedef struct st_point{
+    int x;
+    int y;
+} point;
+
+typedef struct st_button{
+    uint8_t state;
+    char text[GAL_BUTTON_MAXLEN];
+    point pos;
+} button;
+
+typedef struct st_enemy{
+    uint8_t type;//Type of enemy
+    uint8_t state;//State (attacking, passive etc)
+    point pos;//Position
+    int cooldown;//Shooting cooldown
+
+    uint8_t frame;//Current animation frame
+} enemy;
 
 extern uint8_t state, level;
 extern int score, lives;
 extern SDL_Surface *screen;
+extern SDL_Surface *img_player;
+extern SDL_Surface *img_enemy[];
